4cd471bf6826ef155446d2887b990ab154a01350
Fadeevich <fad.danila053@gmail.com>
2022-03-29 22:19:33 +0300

    change name variable in file 'MethodDividingSegmentHalf.cpp'

--- Laboratory_06/MethodDividingSegmentHalf/MethodDividingSegmentHalf.cpp
+++ Laboratory_06/MethodDividingSegmentHalf/MethodDividingSegmentHalf.cpp
@@ -21,12 +21,12 @@ namespace cm
 		utility::BaseType delta = b - a;
 		size_t n = ceil(delta / data->getEpsilon() - 1);
 		
-		utility::BaseType s = delta / 2.1L;
+		utility::BaseType incrementStep = delta / 2.1L;
 		while (delta > data->getEpsilon())
 		{
 			utility::BaseType tmpRes = (a + b) / 2.0L;	
-			utility::BaseType alpha = tmpRes - s;
-			utility::BaseType beta = tmpRes + s;
+			utility::BaseType alpha = tmpRes - incrementStep;
+			utility::BaseType beta = tmpRes + incrementStep;
 
 			if (func(alpha) <= func(beta))
 			{
@@ -38,7 +38,7 @@ namespace cm
 			}
 
 			delta = b - a;
-			s = delta / 2.1L;
+			incrementStep = delta / 2.1L;
 
 			cout << "\ta = " << a << "\n";
 			cout << "\tb = " << b << "\n";
